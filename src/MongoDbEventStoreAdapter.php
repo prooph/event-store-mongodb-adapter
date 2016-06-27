e8d69184de2656ddb7f34f79105d3c91fe21b25e
prolic <saschaprolic@googlemail.com>
2016-06-28 03:54:45 +0800

    better concurrency handling

--- src/MongoDbEventStoreAdapter.php
+++ src/MongoDbEventStoreAdapter.php
@@ -73,7 +73,6 @@ final class MongoDbEventStoreAdapter implements Adapter, CanHandleTransaction
      */
     private $writeConcern = [
         'w' => 1,
-        'j' => true
     ];
 
     /**
@@ -174,8 +173,13 @@ final class MongoDbEventStoreAdapter implements Adapter, CanHandleTransaction
 
         try {
             $insertBatch->execute();
+        } catch (\MongoDuplicateKeyException $e) {
+            throw new ConcurrencyException('At least one event with same version exists already', 0, $e);
         } catch (\MongoWriteConcernException $e) {
-            throw new ConcurrencyException('', 0, $e);
+            $code = $e->getDocument()['writeErrors'][0]['code'];
+            if (in_array($code, [11000, 11001, 12582])) {
+                throw new ConcurrencyException('At least one event with same version exists already', 0, $e);
+            }
         }
     }
 
